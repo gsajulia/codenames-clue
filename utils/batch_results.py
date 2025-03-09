3eea630f513daf550ff0befa4dca951317df95b5
Julia Santi <juliagabriela98@gmail.com>
2025-03-08 22:39:14 -0300

    feat: add fixed batch for testing

--- utils/batch_results.py
+++ utils/batch_results.py
@@ -13,6 +13,83 @@ class BatchResults:
       self.model_glove = GloveModel()
       self.model_bert = BertModel()
       self.board = GenerateBoard()
+      # batch 15
+      self.target_board_8_words = [
+        ['ash', 'future', 'wall', 'wanted', 'fishing', 'hotel', 'seal', 'boy'],
+        ['water', 'salad', 'note', 'link', 'group', 'mole', 'soccer', 'columbus'],
+        ['planets', 'neptune', 'blind', 'root', 'africa', 'life', 'nebula', 'beverage'],
+        ['hot', 'wheel', 'kitchen', 'bolt', 'share', 'internet', 'monkey', 'pyramid'],
+        ['cannon', 'genius', 'balance', 'baseball', 'port', 'toronto', 'spider', 'jinx'],
+        ['magazine', 'duel', 'tuna', 'steel', 'brace', 'comb', 'astronomy', 'spring'],
+        ['farm', 'potato', 'ace', 'resume', 'volume', 'plot', 'track', 'card'],
+        ['small', 'rage', 'cloak', 'alaska', 'taboo', 'turkey', 'saturn', 'basketball'],
+        ['king', 'clown', 'frozen', 'leak', 'fiddle', 'range', 'candle', 'potion'],
+        ['diamond', 'junk', 'drain', 'brazil', 'wet', 'bench', 'chief', 'wax'],
+        ['philippine', 'vegas', 'treat', 'witch', 'magician', 'mile', 'chance', 'agent'],
+        ['cane', 'turn', 'alliance', 'statue', 'date', 'carrot', 'lancelot', 'margin'],
+        ['frankenstein', 'application', 'antarctica', 'pendant', 'kick', 'union', 'jeweler', 'disney'],
+        ['figure', 'post', 'score', 'shack', 'point', 'cook', 'wine', 'viking'],
+        ['dress', 'cinderella','swamp','fat', 'sword', 'sneak', 'comic', 'mushroom']
+      ]
+      
+      self.target_board_2_words = [line[:2] for line in self.target_board_8_words]
+      self.target_board_3_words = [line[:3] for line in self.target_board_8_words]
+      
+    def fixed_batch_results(self, target_board, board_name):
+      i = 0
+      while i < len(target_board):
+        try:
+          default_target = target_board[i]
+          default_avoid = []
+          print("target", default_target)
+          print("\n\n avoid", default_avoid)
+          
+          target_words = default_target
+
+          model_result_bert_embedding_nn = self.model_bert.select_best_hint_from_embeddings_and_neighbors(target_words)
+          print(f"Suggested BERT NN hint: {model_result_bert_embedding_nn["best_hint"]}\n")
+          
+          model_result_bert_embedding = self.model_bert.select_best_hint_from_embeddings(target_words)
+          print(f"Suggested BERT embedding hint: {model_result_bert_embedding["best_hint"]}\n")
+          
+          model_result_glove_embedding_nn  = self.model_glove.select_best_hint_from_embeddings_and_neighbors(target_words, default_avoid)
+          print(f"Suggested Glove NN hint: {model_result_glove_embedding_nn["best_hint"]}\n")
+          
+          model_result_glove_embedding = self.model_glove.select_best_hint_from_embeddings(target_words, default_avoid)
+          print(f"Suggested Glove embedding hint: {model_result_glove_embedding["best_hint"]}\n")
+          self.results.append(
+              {
+                  "Cards target": target_words,
+                  "Expected word NN BERT": model_result_bert_embedding_nn["best_hint"],
+                  "Similarity NN BERT":model_result_bert_embedding_nn["best_score"],
+                  "Expected embedding word BERT": model_result_bert_embedding_nn["best_hint"],
+                  "Similarity embedding BERT":model_result_bert_embedding_nn["best_score"],
+                  "Expected word NN GloVe": model_result_glove_embedding_nn["best_hint"],
+                  "Similarity NN GloVe": model_result_glove_embedding_nn["best_score"],
+                  "Expected word embedding GloVe": model_result_glove_embedding["best_hint"],
+                  "Similarity embedding GloVe": model_result_glove_embedding["best_score"],
+                  "Board": board_name
+                  
+              }
+          )
+          i += 1
+        
+        except Exception as e:
+            print(f"Error {i}: {e}")
+            break
+          
+    def get_fixed_batch_results(self):
+      self.fixed_batch_results(self.target_board_8_words,"board_8")
+      self.fixed_batch_results(self.target_board_2_words,"board_2")
+      self.fixed_batch_results(self.target_board_3_words,"board_3")
+      
+      if self.results:
+          df = pd.DataFrame(self.results)
+          file_name = f"results/{uuid.uuid4()}_results_with_sections.csv"
+          df.to_csv(file_name, index=False)
+          print(f"All results saved in {file_name}")
+      else:
+          print("Empty results.")
       
     def get_batch_results(self):
       i = 0
@@ -36,11 +113,11 @@ class BatchResults:
           
           print("Choosed words BERT", clue_candidates_target_bert)
           model_result_bert = self.model_bert.find_hint(clue_candidates_target_bert, avoid_words)
-          print(f"Suggested Glove hint: {model_result_bert["best_hint"]}\n")
+          print(f"Suggested BERT hint: {model_result_bert["best_hint"]}\n")
           
           print("Choosed words GloVe", clue_candidates_target_glove)
           model_result_glove = self.model_glove.find_hint(clue_candidates_target_glove, avoid_words)
-          print(f"Suggested Bert hint: {model_result_glove["best_hint"]}\n")
+          print(f"Suggested Glove hint: {model_result_glove["best_hint"]}\n")
           self.results.append(
               {
                   "Cards target": target_words,
